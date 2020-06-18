1558b9057674ff82d78c8bb3d9863071ba84deae
nalin_garg <nalin_garg@apple.com>
2020-06-17 18:51:23 -0700

    l3.2: coin

---
+++ coins.py
@@ -0,0 +1,20 @@
+
+print("Please enter the amount of money to convert::\n")
+
+dollars = int(input("# of dollars: "))
+cents = int(input("# of cents: "))
+
+total = (dollars * 100) + cents
+r = total
+
+quaters = int(r / 25)
+r = r % 25
+
+dimes = int(r / 10)
+r = r % 10
+
+nickels = int(r / 5)
+r = r % 5
+
+
+print("The coins are {} quarters, {} dimes, {} nickels and {} pennies".format(quaters, dimes, nickels, r))
